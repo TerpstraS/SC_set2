2eb041647ebe0fb16656d568b1794db43cfc0b91
sjoerd23 <sjoerd.terpstra@student.uva.nl>
2021-02-22 20:22:40 +0100

    created necessary python files

---
+++ dla_monte_carlo.py
@@ -0,0 +1,7 @@
+import numpy as np
+import numba
+
+
+def dla_monte_carlo():
+    """DLA model using Monte Carlo simulation """
+    return
---
+++ dla_prob_model.py
@@ -0,0 +1,8 @@
+import numpy as np
+import numba
+
+
+def dla_prob_model():
+    """DLA model using growth probability """
+
+    return
